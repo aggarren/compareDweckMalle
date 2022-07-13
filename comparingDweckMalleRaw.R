1103e59185f52c78fec50b7d435d923eb4599329
Anna Garren <35542357+aggarren@users.noreply.github.com>
2022-07-13 14:29:12 -0400

    Add files via upload

--- comparingDweckMalleRaw.R
+++ comparingDweckMalleRaw.R
@@ -36,8 +36,12 @@ d_malle <- d_malle[3:nrow(d_malle),1:ncol(d_malle)]
 d_malle <- matrix(as.numeric(d_malle),ncol=ncol(d_malle))
 d_dweck <- as.data.frame(d_dweck)
 d_malle <- as.data.frame(d_malle)
-## function to cycle through participants for all characters and gather stats ---------------
-analyze <- function(matrix1,matrix2){
+## function to analyze differences between two data sets
+analyze <- function(data1,data2){
+  
+}
+## function to cycle through participants for all characters ---------------
+strip <- function(matrix1,matrix2){
   listN <- c(17,23,22,22,20,19,20,20,21,18,20,24,20,22,21,21,20,21,19,20,21)
   index <- 1
   for(character in 1:length(listN)){
@@ -45,7 +49,8 @@ analyze <- function(matrix1,matrix2){
     currData1 <- matrix1[index:N,]
     currData2 <- matrix2[index:N,]
     index <- index + N
+    analyze(currData1,currData2)
   }
 }
 ## perform analysis on data
-analyze(d_dweck,d_malle)
+strip(d_dweck,d_malle)
