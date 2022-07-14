e44c718f7480ba34bd4132102b9cbd4e4572f515
Anna Garren <35542357+aggarren@users.noreply.github.com>
2022-07-14 10:12:32 -0400

    Add files via upload

--- comparingDweckMalleRaw.R
+++ comparingDweckMalleRaw.R
@@ -31,27 +31,69 @@ if (!exists ("d_dweck") || !exists("d_malle"))
 d_dweck <- as.matrix(d_dweck)
 d_dweck <- d_dweck[,2:ncol(d_dweck)]
 d_dweck <- matrix(as.numeric(d_dweck),ncol=ncol(d_dweck))
+d_malle <- d_malle[1:nrow(d_malle),1:ncol(d_malle)]
 d_malle <- as.matrix(t(d_malle))
-d_malle <- d_malle[3:nrow(d_malle),1:ncol(d_malle)]
 d_malle <- matrix(as.numeric(d_malle),ncol=ncol(d_malle))
 d_dweck <- as.data.frame(d_dweck)
-d_malle <- as.data.frame(d_malle)
-## function to analyze differences between two data sets
+d_malle <- as.data.frame(d_malle[3:nrow(d_malle),])
+d_dweck <- mutate_all(d_dweck,function(x) as.numeric(as.character(x)))
+d_malle <- mutate_all(d_malle,function(x) as.numeric(as.character(x)))
+## functions to analyze differences between two data sets
+minimum <- function(data){
+  results <- c()
+  for(index in 1:nrow(data)){
+    append(results,min(unlist(data[index,])))
+  }
+  return(results)
+}
+maximum <- function(data){
+  results <- c()
+  for(index in 1:nrow(data)){
+    append(results,max(unlist(data[index,])))
+  }
+  return(results)
+}
+meanValue <- function(data){
+  results <- c()
+  for(index in 1:nrow(data)){
+    append(results,mean(unlist(data[index,])))
+  }
+  return(results)
+}
+medianValue <- function(data){
+  results <- c()
+  for(index in 1:nrow(data)){
+    append(results,median(unlist(data[index,])))
+  }
+  return(results)
+}
+standardDev <- function(data){
+  results <- c()
+  for(index in 1:nrow(data)){
+    append(results,sd(unlist(data[index,])))
+  }
+  return(results)
+}
 analyze <- function(data1,data2){
-  stats <- 6
-  results <- matrix(data=NA,nrow=40,ncol=(stats*2))
   ##min
-  
+  min1 <- minimum(data1)
+  min2 <- minimum(data2)
   ##max
-  
+  max1 <- maximum(data1)
+  max2 <- maximum(data2)
   ##mean
-  
+  mean1 <- meanValue(data1)
+  mean2 <- meanValue(data2)
   ##median
-  
+  median1 <- medianValue(data1)
+  median2 <- medianValue(data2)
   ##standard deviation
+  sd1 <- standardDev(data1)
+  sd2 <- standardDev(data2)
   
-  ##Rsquared
-  
+  results1 <- data.frame(min1,max1,mean1,median1,sd1)
+  results2 <- data.frame(min2,max2,mean2,median2,sd2)
+  results <- results1-results2
   return(results)
 }
 ## function to cycle through participants for all characters ---------------
@@ -64,8 +106,10 @@ strip <- function(matrix1,matrix2){
     currData2 <- matrix2[index:N,]
     index <- index + N
     results <- analyze(currData1,currData2)
-    write.csv(results,paste0("statsComparison",character))
+    if(character == 1){ final <- results}
+    else { final <- cbind(final,results)}
   }
+  write.csv(final,paste0(dataDirectory,"comparison.csv"))
 }
 ## perform analysis on data--------------------
 strip(d_dweck,d_malle)
