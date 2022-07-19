ad7983209dcadfa439fb49b6898795da7eaba9b5
Anna Garren <35542357+aggarren@users.noreply.github.com>
2022-07-19 15:26:26 -0400

    Add files via upload

---
+++ compareItemCharNums.R
@@ -0,0 +1,25 @@
+library(psych)
+data <- read.csv("items16chars3.csv")
+model <- principal(data,nfactors=3)
+sixteenThree <- model$loadings
+
+data <- read.csv("items16chars21.csv")
+model <- principal(data,nfactors=3)
+sixteenTwentyOne <- model$loadings
+
+data <- read.csv("items40chars3.csv")
+model <- principal(data,nfactors=3)
+fourtyThree <- model$loadings
+
+data <- read.csv("items40chars21.csv")
+model <- principal(data,nfactors=3)
+fourtyTwentyOne <- model$loadings
+
+sixteenDifference <- sixteenThree - sixteenTwentyOne
+fourtyDifference <- fourtyThree - fourtyTwentyOne
+sixteenDifference
+#big difference when increasing character
+  #3 to 21 when using 16 items
+fourtyDifference 
+#no difference when increasing character
+  #3 to 21 when using 40 items
