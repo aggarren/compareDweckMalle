9038e1a559c50a52a923d40ed0301df6b451d95b
Anna Garren <35542357+aggarren@users.noreply.github.com>
2022-07-20 13:07:13 -0400

    Add files via upload

--- compareItemCharNums.R
+++ compareItemCharNums.R
@@ -3,27 +3,28 @@ data <- read.csv("items16chars3.csv")
 data <- data[,2:ncol(data)]
 model <- principal(data,nfactors=3)
 sixteenThree <- model$loadings
+write.csv(sixteenThree,"sixteenThreeLoadings.csv")
 
 data <- read.csv("items16chars21.csv")
 data <- data[,2:ncol(data)]
 model <- principal(data,nfactors=3)
 sixteenTwentyOne <- model$loadings
+write.csv(sixteenTwentyOne,"sixteenTwentyOneLoadings.csv")
 
 data <- read.csv("items40chars3.csv")
 data <- data[,2:ncol(data)]
 model <- principal(data,nfactors=3)
 fourtyThree <- model$loadings
+write.csv(fourtyThree,"fourtyThreeLoadings.csv")
+
 
 data <- read.csv("items40chars21.csv")
 data <- data[,2:ncol(data)]
 model <- principal(data,nfactors=3)
 fourtyTwentyOne <- model$loadings
+write.csv(fourtyTwentyOne,"fourtyTwentyOneLoadings.csv")
 
 sixteenDifference <- sixteenThree - sixteenTwentyOne
 fourtyDifference <- fourtyThree - fourtyTwentyOne
-sixteenDifference
-#big difference when increasing character
-  #3 to 21 when using 16 items
-fourtyDifference 
-#no difference when increasing character
-  #3 to 21 when using 40 items
+write.csv(sixteenDifference,"sixteenDifference.csv")
+write.csv(fourtyDifference,"fourtyDifference.csv")
