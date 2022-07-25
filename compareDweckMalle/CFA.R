caf75bf004dcc848413fe7de5212b044d278d9a2
Anna Garren <35542357+aggarren@users.noreply.github.com>
2022-07-25 13:48:24 -0400

    Add files via upload

--- compareDweckMalle/CFA.R
+++ compareDweckMalle/CFA.R
@@ -18,4 +18,32 @@ VerifyPathIsSafe(processedDataDirectory)
 if (!exists ("df_dweck"))
   source(paste0(workingDirectory, experimentName, "/R/", "GetData.R"))
 
+m_40_21 <- 'RC1 =~ V24 + V25 + V26 + V27 + V28 +
+V29 + V30 + V31 + V32 + V33 + V34 + V35 + V36 +
+V37 + V38 + V39 + V40
+RC2 =~ V11 + V12 + V13 + V14 + V15 + V16 + V17 +
+V18 + V19 + V20 + V21
+RC3 =~ V1 + V2 + V3 + V4 + V5 + V6 + V7 + V8 + V9'
+cfa_40_21 <- cfa(m_40_21,data=df_40_21,std.lv=TRUE)
+all_40_21 <- summary(cfa_40_21,fit.measures=TRUE,standardized=TRUE)
 
+m_16_21 <- 'RC1 =~ V1 + V2 + V3 + V4 + V5 + V6 + V7
+RC2 =~ V8 + V10 + V11 + V13
+RC3 =~ V12 + V14 + V15 + V16'
+cfa_16_21 <- cfa(m_16_21,data=df_16_21,std.lv=TRUE)
+all_16_21 <- summary(cfa_16_21,fit.measures=TRUE,standardized=TRUE)
+
+m_40_3 <- 'RC1 =~ V16 + V17 + V18 + V19 + V20 +
+V21 + V22 + V24 + V25 + V26 + V27 + V28 +
+V29 + V30 + V31 + V32 + V33 + V34 + V35 + V36 +
+V37 + V38 + V39 + V40
+RC2 =~ V2 + V4 + V7 + V8 + V9
+RC3 =~ V1 + V10 + V13 + V14'
+cfa_40_3 <- cfa(m_40_3,data=df_40_3,std.lv=TRUE)
+all_40_3 <- summary(cfa_40_3,fit.measures=TRUE,standardized=TRUE)
+
+m_16_3 <- 'RC1 =~ V1 + V2 + V3 + V4 + V5 + V6 + V7
+RC2 =~ V14 + V15 + V16
+RC3 =~ V10 + V12 + V13'
+cfa_16_3 <- cfa(m_16_3,data=df_16_3,std.lv=TRUE)
+all_16_3 <- summary(cfa_16_3,fit.measures=TRUE,standardized=TRUE)
